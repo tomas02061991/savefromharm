9b76abe390cdfafea79a220b19ce91f2b5937e0b
tomas02061991 <tomas02061991@gmail.com>
2016-02-25 16:23:42 +0100

    renames styles.css to style.css

--- wp-content/themes/safefromharm/header.php
+++ wp-content/themes/safefromharm/header.php
@@ -10,7 +10,7 @@
         <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
         <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-        <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/styles.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/style.css">
 	</head>
     
 	<body>
