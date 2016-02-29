02430b250eee592c99c25e13a74bc3bd9344b3c8
tomas02061991 <tomas02061991@gmail.com>
2016-02-29 14:35:36 +0100

    Adds donate modal

--- wp-content/themes/safefromharm/header.php
+++ wp-content/themes/safefromharm/header.php
@@ -23,5 +23,5 @@
                 </a>
             </div>
             <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-            <div id="donate">DONATE</div>
+            <button id="donate" data-toggle="modal" data-target="#donateModal">DONATE</button>
 		</nav>
