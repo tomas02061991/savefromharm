6f9be791fd32e0ffbaf5e0cd3213df738dfd84c5
tomas02061991 <tomas02061991@gmail.com>
2016-02-29 19:35:32 +0100

    put form in

--- wp-content/themes/safefromharm/index.php
+++ wp-content/themes/safefromharm/index.php
@@ -100,7 +100,43 @@
                     <h4 class="modal-title" id="myModalLabel">DONATE NOW!</h4>
                   </div>
                   <div class="modal-body">
-                    ...
+                    <form action="">
+                        <label>Full Name:</label>
+                        <input type="text" name="name" placeholder="John Johnson" required>
+                        <label>Email:</label>
+                        <input type="email" name="email" placeholder="example@example.com" required>
+                        <label>Card Number:</label>
+                        <input type="number" name="card" placeholder="xxxxxxxxxxxxxxxx" required>
+                        <label>Expiration Date:</label>
+                        <select name="exmonth" required>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                            <option>6</option>
+                            <option>7</option>
+                            <option>8</option>
+                            <option>9</option>
+                            <option>10</option>
+                            <option>11</option>
+                            <option>12</option>
+                        </select>
+                        <select name="exyear" required>
+                            <option>16</option>
+                            <option>17</option>
+                            <option>18</option>
+                            <option>19</option>
+                            <option>20</option>
+                            <option>21</option>
+                            <option>22</option>
+                            <option>23</option>
+                            <option>24</option>
+                        </select>
+                        <label>CVC Number:</label>
+                        <input type="number" name="card" placeholder="xxxx" required>
+                        <input type="submit" value="Donate Now">
+                    </form>
                   </div>
                   <div class="modal-footer">
                     <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
