832440b55b845e04672b0cd9f44819fa6916adbd
tomas02061991 <tomas02061991@gmail.com>
2016-02-29 10:32:37 +0100

    changes header

--- wp-content/themes/safefromharm/header.php
+++ wp-content/themes/safefromharm/header.php
@@ -12,7 +12,6 @@
         <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
         <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/style.css">
-	<?php wp_head(); ?>
     </head>
     
 	<body <?php body_class(); ?>>
