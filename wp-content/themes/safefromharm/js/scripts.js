0950c82405dd4e092ef479a6b92348c1e9bda34c
tomas02061991 <tomas02061991@gmail.com>
2016-03-01 13:51:22 +0100

    fixes menu color

--- wp-content/themes/safefromharm/js/scripts.js
+++ wp-content/themes/safefromharm/js/scripts.js
@@ -28,13 +28,18 @@ $(document).on("ready", function () {
                 $("nav").css("background-color", "rgba(0, 0, 0, 0.5)")
                 $("nav ul li a").css("color", "#ffffff")
                 $("nav .logo").attr("src", themePath+"images/logo_hvid.png")
-                $(".navbar-default .navbar-collapse, .navbar-default .navbar-form").css({"background-color": "rgba(0, 0, 0, 0.5)", "border-color":"#fff"});
+                if($(".navbar-collapse").attr("area-expand")){
+                    $(".navbar-default .navbar-collapse, .navbar-default .navbar-form").css({"background-color": "rgba(0, 0, 0, 0.5)", "border-color":"#fff"});
+                }
             }
             if (position <= 5 && bgColor == "rgb(255, 255, 255)") {
                 $("nav").css("background-color", "rgba(255, 255, 255, 0.5)")
                 $("nav ul li a").css("color", "#010f1a")
                 $("nav .logo").attr("src", themePath+"images/logo_sort.png")
-                $(".navbar-default .navbar-collapse, .navbar-default .navbar-form").css({"background-color": "rgba(255, 255, 255, 0.5)", "border-color":"#000"});
+                if($(".navbar-collapse").attr("area-expand")){
+                    $(".navbar-default .navbar-collapse, .navbar-default .navbar-form").css({"background-color": "rgba(255, 255, 255, 0.5)", "border-color":"#000"});
+                }
+               
             }
         });        
     });
