91e451046d08d90f9b680a61660996926f624170
Camilleon <cklcam@hotmail.com>
2016-03-03 00:12:09 +0100

    lightbox text

--- wp-content/themes/safefromharm/lightboxes.php
+++ wp-content/themes/safefromharm/lightboxes.php
@@ -6,7 +6,7 @@
         <h4 class="modal-title" id="myModalLabel">Bengal Tiger</h4>
       </div>
       <div class="modal-body col-xs-12">
-          <div class="col-xs-12"><img style="width: 100%;" src="http://safefromharm.cryolab.dk/wp-content/themes/safefromharm/images/bengal_tiger1.png" alt="Bengal tiger">
+          <div class="col-xs-12"><img style="width: 100%;" src="http://safefromharm.cryolab.dk/wp-content/themes/safefromharm/images/lightboxtiger1.jpg" alt="Bengal tiger">
           </div>
           <div class="col-xs-12">
               <p>The Bengal tiger is found primarily in India with smaller populations in Bangladesh, Nepal, Bhutan, China and Myanmar. It is the most numerous of all tiger subspecies with more than 2,500 left in the wild. The creation of India’s tiger reserves in the 1970s helped to stabilize numbers, but poaching to meet a growing demand from Asia in recent years has once again put the Bengal tiger at risk. The mangroves of the Sundarbans—shared between Bangladesh and India—are the only mangrove forests where tigers are found. The Sundarbans are increasingly threatened by sea level rise as a result of climate change.</p>
@@ -26,8 +26,16 @@
       <div class="modal-header">
         <h4 class="modal-title" id="myModalLabel">Amur Tiger</h4>
       </div>
-      <div class="modal-body">
-        <!-- content here -->
+      <div class="modal-body col-xs-12">
+          <div class="col-xs-12"><img style="width: 100%;" src="http://safefromharm.cryolab.dk/wp-content/themes/safefromharm/images/lightboxtiger2.jpg" alt="Bengal tiger">
+          </div>
+          <div class="col-xs-12">
+              <p>Amur tigers were once found throughout the Russian Far East, northern China and the Korean peninsula. By the 1940s, hunting had driven the Amur tiger to the brink of extinction—with no more than 40 individuals remaining in the wild. The subspecies was saved when Russia became the first country in the world to grant the tiger full protection.<br><br>
+
+              By the 1980s, the Amur tiger population had increased to around 500. Although poaching increased after the collapse of the Soviet Union, continued conservation and antipoaching efforts by many partners—including WWF—have helped keep the population stable at around 540 individuals.<br><br> 
+
+              Amur tigers have the largest home range of any tiger subspecies because they have to search over large areas to find food due to low prey densities. They represent the largest unfragmented tiger population in the world.</p>
+          </div>          
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -43,8 +51,12 @@
       <div class="modal-header">
         <h4 class="modal-title" id="myModalLabel">Malayan Tiger</h4>
       </div>
-      <div class="modal-body">
-        <!-- content here -->
+      <div class="modal-body col-xs-12">
+          <div class="col-xs-12"><img style="width: 100%;" src="http://safefromharm.cryolab.dk/wp-content/themes/safefromharm/images/lightboxtiger3.jpg" alt="Bengal tiger">
+          </div>
+          <div class="col-xs-12">
+              <p>Malayan tigers were classified as Indochinese tigers until DNA testing in 2004 showed them to be a separate subspecies. Their Latin name—Panthera tigris jacksoni—honors Peter Jackson, the famous tiger conservationist. Malayan tigers are found only on the Malay Peninsula and in the southern tip of Thailand.</p>
+          </div>          
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -60,8 +72,14 @@
       <div class="modal-header">
         <h4 class="modal-title" id="myModalLabel">Sumatran Tiger</h4>
       </div>
-      <div class="modal-body">
-        <!-- content here -->
+      <div class="modal-body col-xs-12">
+          <div class="col-xs-12"><img style="width: 100%;" src="http://safefromharm.cryolab.dk/wp-content/themes/safefromharm/images/lightboxtiger4.jpg" alt="Bengal tiger">
+          </div>
+          <div class="col-xs-12">
+              <p>Sumatran tigers are the smallest surviving tiger subspecies and are distinguished by heavy black stripes on their orange coats. The last of Indonesia’s tigers—now fewer than 400—are holding on for survival in the remaining patches of forests on the island of Sumatra. Accelerating deforestation and rampant poaching mean this noble creature could end up like its extinct Javan and Balinese relatives.<br><br>
+
+              In Indonesia, anyone caught hunting tigers could face jail time and steep fines. But despite increased efforts in tiger conservation—including strengthening law enforcement and antipoaching capacity—a substantial market remains in Sumatra and the rest of Asia for tiger parts and products. Sumatran tigers are losing their habitat and prey fast, and poaching shows no sign of decline.</p>
+          </div>          
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -77,8 +95,14 @@
       <div class="modal-header">
         <h4 class="modal-title" id="myModalLabel">Indochinese Tiger</h4>
       </div>
-      <div class="modal-body">
-        <!-- content here -->
+      <div class="modal-body col-xs-12">
+          <div class="col-xs-12"><img style="width: 100%;" src="http://safefromharm.cryolab.dk/wp-content/themes/safefromharm/images/lightboxtiger5.jpg" alt="Bengal tiger">
+          </div>
+          <div class="col-xs-12">
+              <p>There is hope in other remaining Indochinese tiger habitats, which have a relatively low human presence and offer a unique opportunity for tiger conservation. The best hope of the survival of this subspecies is in the Dawna Tennaserim landscape on the Thailand-Myanmar border where perhaps 250 tigers remain. WWF considers the forests of the Lower Mekong a restoration landscape with the possibility of reintroducing tigers as the habitat and prey base are there. Southern Laos and Central Vietnam also have potential for recovery of wild tiger populations.<br><br>
+
+              Access to the areas where Indochinese tigers live is often restricted, and biologists have only recently been granted limited permits for field surveys. As a result, there is still much to learn about the status of these tigers in the wild.</p>
+          </div>          
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
@@ -94,8 +118,14 @@
       <div class="modal-header">
         <h4 class="modal-title" id="myModalLabel">South China Tiger</h4>
       </div>
-      <div class="modal-body">
-        <!-- content here -->
+      <div class="modal-body col-xs-12">
+          <div class="col-xs-12"><img style="width: 100%;" src="http://safefromharm.cryolab.dk/wp-content/themes/safefromharm/images/lightboxtiger6.jpg" alt="Bengal tiger">
+          </div>
+          <div class="col-xs-12">
+              <p>The South China tiger population was estimated to number 4,000 individuals in the early 1950s. In the next few decades, thousands were killed as the subspecies was hunted as a pest. The Chinese government banned hunting in 1979. By 1996 the population was estimated to be just 30-80 individuals.
+
+              Today the South China tiger is considered by scientists to be “functionally extinct,” as it has not been sighted in the wild for more than 25 years.</p>
+          </div>          
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
