90bc0c4a82d121fed618bb872dfc87580a86e35d
tomas02061991 <tomas02061991@gmail.com>
2016-03-02 22:57:51 +0100

    adds lightboxes.php

---
+++ wp-content/themes/safefromharm/lightboxes.php
@@ -0,0 +1,101 @@
+<!-- tiger1 -->
+<div class="modal fade" id="tiger1" tabindex="-1" role="dialog" aria-labelledby="tiger1">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title" id="myModalLabel">DONATE NOW!</h4>
+      </div>
+      <div class="modal-body">
+        <!-- content here -->
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- tiger2 -->
+<div class="modal fade" id="tiger2" tabindex="-1" role="dialog" aria-labelledby="tiger2">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title" id="myModalLabel">DONATE NOW!</h4>
+      </div>
+      <div class="modal-body">
+        <!-- content here -->
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- tiger3 -->
+<div class="modal fade" id="tiger3" tabindex="-1" role="dialog" aria-labelledby="tiger3">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title" id="myModalLabel">DONATE NOW!</h4>
+      </div>
+      <div class="modal-body">
+        <!-- content here -->
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- tiger4 -->
+<div class="modal fade" id="tiger4" tabindex="-1" role="dialog" aria-labelledby="tiger4">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title" id="myModalLabel">DONATE NOW!</h4>
+      </div>
+      <div class="modal-body">
+        <!-- content here -->
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- tiger5 -->
+<div class="modal fade" id="tiger5" tabindex="-1" role="dialog" aria-labelledby="tiger5">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title" id="myModalLabel">DONATE NOW!</h4>
+      </div>
+      <div class="modal-body">
+        <!-- content here -->
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- tiger6 -->
+<div class="modal fade" id="tiger6" tabindex="-1" role="dialog" aria-labelledby="tiger6">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title" id="myModalLabel">DONATE NOW!</h4>
+      </div>
+      <div class="modal-body">
+        <!-- content here -->
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
