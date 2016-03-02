877942fc75acadb5d65f4d6902b8c0f0fe876c5c
tomas02061991 <tomas02061991@gmail.com>
2016-03-02 14:00:58 +0100

    configure bxslider

--- wp-content/themes/safefromharm/js/scripts.js
+++ wp-content/themes/safefromharm/js/scripts.js
@@ -2,7 +2,7 @@ const themePath = "http://safefromharm.cryolab.dk/wp-content/themes/safefromharm
 
 $(document).on("ready", function () {
 
-    $('.bxslider').bxSlider({slideWidth:500});
+    $('.bxslider').bxSlider({slideWidth:5000});
 
     $("#menu-primary").removeClass("menu");
     $("#menu-primary").addClass("nav navbar-nav");
