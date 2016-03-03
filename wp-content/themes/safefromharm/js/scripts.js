7dd977567032e522a7ee074609df59fd8a2279b0
tomas02061991 <tomas02061991@gmail.com>
2016-03-03 09:36:32 +0100

    changes map fading

--- wp-content/themes/safefromharm/js/scripts.js
+++ wp-content/themes/safefromharm/js/scripts.js
@@ -7,13 +7,11 @@ $(document).on("ready", function () {
     $("#mapbtn").on("click", function(){
         if($(this).html() == "BEFORE"){
             $(this).html("AFTER");
-            $("#map img").fadeOut();
-            $("#map img").attr("src", "http://safefromharm.cryolab.dk/wp-content/themes/safefromharm/images/themap_now.jpg");
+            $("#map img").fadeOut(0.5, function(){$("#map img").attr("src", "http://safefromharm.cryolab.dk/wp-content/themes/safefromharm/images/themap_now.jpg");});
             $("#map img").fadeIn();
         } else{
             $(this).html("BEFORE");
-            $("#map img").fadeOut();
-            $("#map img").attr("src", "");
+            $("#map img").fadeOut(0.5, function(){$("#map img").attr("src", "");});
             $("#map img").fadeIn();
         }
     });
